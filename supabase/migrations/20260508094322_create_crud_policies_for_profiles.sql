5fce6ed6e471bbc9835e14cab1695cf722ef604f
Le Vady <levady.sinarta@gmail.com>
2026-05-08 16:56:58 +0700

    create policies for profiles table

---
+++ supabase/migrations/20260508094322_create_crud_policies_for_profiles.sql
@@ -0,0 +1,11 @@
+drop policy if exists "Instructor can view their own profile" on "public".profiles;
+create policy "Instructor can view their own profile"
+on profiles for select
+using ( true );
+
+drop policy if EXISTS "Instructor can update their own profile" on "public".profiles;
+create policy "Instructor can update their own profile"
+on profiles for update
+to authenticated
+using ( (auth.uid()) = profiles.id )
+with check ( (auth.uid()) = profiles.id );
